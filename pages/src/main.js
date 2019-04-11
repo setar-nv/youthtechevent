b53b0c7b2849b8be15a9de684416083535c73b4f
Abdullah Al-Omari <abdullah.al-omari@setarnv.local>
2019-04-11 10:59:06 -0400

    bootstrap style

--- pages/src/main.js
+++ pages/src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store'
 window.$ = window.jQuery = require('jquery');
 import 'bootstrap3'
-import 'bootstrap3/dist/css/bootstrap.min.css'
+//import 'bootstrap3/dist/css/bootstrap.min.css'
+import './styling/bootstrap.custom.min.css'
 import './styling/main.scss';
 
 Vue.config.productionTip = false
